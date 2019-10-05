480d7228063f718cd65e3af9f5666ee754d729b5
Alex <dsa1314@gmail.com>
2019-10-05 23:29:28 +0800

    🛠 測試演算法

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -25,7 +25,7 @@ const checkResolvable = ary=> {
   })([...ary])
   /** 逆序列數 */
   const count = countComputed(newAry)
-  return count % 2 + spaceX % 2 === 1
+  return count % 2 + spaceX % 2 !== 0
 }
 /** 逆序列累加 */
 const countComputed = ary => {
